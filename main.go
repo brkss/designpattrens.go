0bd9446486003a6b751f9f00c07da2181a1c0562
Brahim Berkasse <berkassebrahim@gmail.com>
2022-12-13 22:25:27 +0100

    🔎 set observer !

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"sync"
+	"time"
 
 	"github.com/brkss/designpattrens.go/decorator"
 	"github.com/brkss/designpattrens.go/generator"
+	"github.com/brkss/designpattrens.go/observer"
 )
 
 
@@ -26,8 +27,30 @@ func main(){
 
 
   // generator 
+ 
+  sub := observer.EventSubject{
+    Observers: sync.Map{},
+  }
+
+  var obs1 = observer.EventObserver{Id: 1, Time: time.Now()}
+  var obs2 = observer.EventObserver{Id: 2, Time: time.Now()}
+
+  sub.AddListener(&obs1)
+  sub.AddListener(&obs2)
+  
+  // remove observer after x time 
+  go func(){
+    select {
+      case <- time.After(time.Microsecond * 10): {
+        sub.RemoveListener(&obs1) 
+      }
+    }
+  }()
+
   for i := range generator.Fib(100000){
-    fmt.Println(i)
+    sub.Notify(observer.Event{Data: i})
+    time.Sleep(time.Microsecond * 10)
+    //fmt.Println(i)
   }
 
 }
--- observer/observer.go
+++ observer/observer.go
@@ -1,5 +1,55 @@
 package observer
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+// Observe Fib function and how long it take to produce new value
 
+type Event struct {
+  Data int
+}
 
+type Observer interface {
+  NotifyCallback(Event)
+}
+
+type Subject interface {
+  AddListener(Observer)
+  RemoveListener(Observer)
+  Notify(Event)
+}
+
+type EventObserver struct {
+  Id int
+  Time time.Time
+}
+
+type EventSubject struct {
+  Observers sync.Map
+}
+
+func (e *EventObserver) NotifyCallback(event Event){
+  fmt.Printf("Observer : %d, \tRecieved: %d \tafter %v\n", e.Id, event.Data, time.Since(e.Time))
+}
+
+func (s *EventSubject) AddListener(obs Observer){
+  s.Observers.Store(obs, struct{}{})
+}
+
+func (s *EventSubject) RemoveListener(obs Observer){
+  s.Observers.Delete(obs)
+}
+
+func (s *EventSubject) Notify(event Event){
+  s.Observers.Range(func(key interface{}, value interface{}) bool {
+    if key == nil || value == nil {
+      return false;
+    }
+
+    key.(Observer).NotifyCallback(event)
+    return true;
+  })
+}
